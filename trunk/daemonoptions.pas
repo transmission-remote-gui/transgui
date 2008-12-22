dd6a8cf2f0efe23257baaa5faf61eab56347b9d7
jura@cp-lab.com <jura@cp-lab.com@3e0809ac-cb9a-11dd-a880-a1081c7ac358>
2008-12-22 17:20:22 +0000

    * Added peer limit setting.

--- trunk/daemonoptions.pas
+++ trunk/daemonoptions.pas
@@ -1,3 +1,22 @@
+{*************************************************************************************
+  This file is part of Transmission Remote GUI.
+  Copyright (c) 2008 by Yury Sidorov.
+
+  Transmission Remote GUI is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  Transmission Remote GUI is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Transmission Remote GUI; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*************************************************************************************}
+
 unit DaemonOptions;
 
 {$mode objfpc}{$H+}
@@ -26,6 +45,8 @@ type
     GroupBox1: TGroupBox;
     Label1: TLabel;
     Label2: TLabel;
+    edMaxPeers: TSpinEdit;
+    txPeerLimit: TLabel;
     txEncryption: TLabel;
     txPort: TLabel;
     edPort: TSpinEdit;
