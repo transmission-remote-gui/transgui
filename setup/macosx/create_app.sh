d8deef4c53d5f0fa3fbee6556921fa53c1f7c1f1
jura@cp-lab.com <jura@cp-lab.com@3e0809ac-cb9a-11dd-a880-a1081c7ac358>
2010-05-02 07:50:39 +0000

    * Script to create MacOS X universal application.

---
+++ setup/macosx/create_app.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+exename=../../transgui
+appname="Transmission Remote GUI"
+appfolder=../../$appname.app
+lazdir=$1
+
+if [ "$lazdir" = "" ]
+then
+  echo "Lazarus dir was not specified."
+  exit 1
+fi
+
+# Building Intel version
+make -C ../.. clean LAZARUS_DIR=$lazdir CPU_TARGET=i386
+make -C ../.. LAZARUS_DIR=$lazdir CPU_TARGET=i386
+strip $exename
+mv $exename $exename.386
+
+# Building PowerPC version
+make -C ../.. clean LAZARUS_DIR=$lazdir CPU_TARGET=powerpc
+make -C ../.. LAZARUS_DIR=$lazdir CPU_TARGET=powerpc
+strip $exename
+mv $exename $exename.ppc
+
+# Creating universal executable
+lipo -create $exename.ppc $exename.386 -output $exename
+rm $exename.386
+rm $exename.ppc
+
+if ! [ -e $exename ]
+then
+  echo "$exename does not exist"
+  exit 1
+fi
+if [ -e "$appfolder" ]
+then
+  rm -r "$appfolder"
+fi
+
+echo "Creating $appfolder..."
+mkdir "$appfolder"
+mkdir "$appfolder/Contents"
+mkdir "$appfolder/Contents/MacOS"
+mkdir "$appfolder/Contents/Resources"
+
+cp $exename "$appfolder/Contents/MacOS"
+cp Info.plist "$appfolder/Contents"
+cp PkgInfo "$appfolder/Contents"
+cp transgui.icns "$appfolder/Contents/Resources"
