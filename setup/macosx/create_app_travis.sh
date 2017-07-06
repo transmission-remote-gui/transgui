68f60967464d2e6c95068ae14f0726d65d1774ab
Peter Dave Hello <hsu@peterdavehello.org>
2017-07-07 06:41:39 +0800

    Fix setup/macosx/create_app_travis.sh

--- setup/macosx/create_app_travis.sh
+++ setup/macosx/create_app_travis.sh
@@ -43,7 +43,7 @@ cp ../../readme.txt "$dmgfolder"
 
 cp PkgInfo "$appfolder/Contents"
 cp transgui.icns "$appfolder/Contents/Resources"
-sed -e "s/@prog_ver@/$prog_ver/" cat Info.plist > "$appfolder/Contents/Info.plist"
+sed -e "s/@prog_ver@/$prog_ver/" Info.plist > "$appfolder/Contents/Info.plist"
 
 hdiutil create -ov -anyowners -volname "transgui-$prog_ver" -imagekey zlib-level=9 -format UDZO -srcfolder ./Release "transgui-$prog_ver.dmg"
 
