cb3dde11c21da44e186e3495c68e1c862b1cd320
Peter Dave Hello <hsu@peterdavehello.org>
2017-07-10 06:43:14 +0800

    Also use git commit hash as build serial in Debian/Ubuntu build script

--- setup/debian-ubuntu/build.sh
+++ setup/debian-ubuntu/build.sh
@@ -4,5 +4,11 @@ set -ex
 
 ROOT="$(cd "$(dirname "$(realpath "${BASH_SOURCE[0]}" )")/../../" && pwd)"
 
+build="$(git rev-list --abbrev-commit --max-count=1 HEAD)"
+
+sed -i.bak "s/'Version %s'/'Version %s Build $build'/" "$ROOT/about.lfm"
+
 make -C "$ROOT" -j"$(nproc)"
 make -C "$ROOT" -j"$(nproc)" zipdist
+
+mv "$ROOT/about.lfm.bak" "$ROOT/about.lfm"
