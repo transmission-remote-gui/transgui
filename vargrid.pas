4983a0f6d4ae1a7ca484f7ee1a8d7139c18bfe39
antekgla <bk11t763@hotmail.com>
2018-03-31 15:07:57 -0300

    Fix (definitely I hope) #1062
    Now works in 1.6 & 1,8 and when scroll is setted one page at the time also.

--- vargrid.pas
+++ vargrid.pas
@@ -127,11 +127,9 @@ type
     procedure SetupCell(ACol, ARow: integer; AState: TGridDrawState; out CellAttribs: TCellAttributes);
     procedure DoOnCheckBoxClick(ACol, ARow: integer);
     procedure DoOnTreeButtonClick(ACol, ARow: integer);
-    {$IF LCL_FULLVERSION < 1080000}
     function  DoMouseWheelDown(Shift: TShiftState; MousePos: TPoint): Boolean; override;
     function  DoMouseWheelUp(Shift: TShiftState; MousePos: TPoint): Boolean; override;
     function  DoMouseWheel(Shift: TShiftState; WheelDelta: Integer; MousePos: TPoint): Boolean; override;
-    {$ENDIF}
     procedure DrawRow(aRow: Integer); override;
     function  GetCells(ACol, ARow: Integer): string; override;
     function  GetEditText(ACol, ARow: Longint): string; override;
@@ -1125,8 +1123,6 @@ begin
     FOnTreeButtonClick(Self, ACol - FixedCols, ARow - FixedRows, ColToDataCol(ACol));
 end;
 
-{$IF LCL_FULLVERSION < 1080000}
-
 function TVarGrid.DoMouseWheelDown(Shift: TShiftState; MousePos: TPoint): Boolean;
 begin
   Result := False;
@@ -1146,14 +1142,19 @@ begin
   Result:=inherited DoMouseWheel(Shift, WheelDelta, MousePos);
   if not Result then begin
     if Mouse.WheelScrollLines = -1 then
+  {$IF LCL_FULLVERSION < 1080000}
       GridMouseWheel(Shift, -WheelDelta*VisibleRowCount div 120)
     else
       GridMouseWheel(Shift, -WheelDelta*Mouse.WheelScrollLines div 120);
-    Result := True;
+  {$ENDIF}
+  {$IF LCL_FULLVERSION >= 1080000}
+      GridMouseWheel(Shift, WheelDelta*VisibleRowCount div 120)
+    else
+      GridMouseWheel(Shift, -WheelDelta div 120);
+  {$ENDIF}
+      Result := True;
   end;
 end;
-
-{$ENDIF}
 
 constructor TVarGrid.Create(AOwner: TComponent);
 begin
