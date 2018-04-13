984fc8705827e6858571aef29bc5e33212a6ecbf
Peter Dave Hello <hsu@peterdavehello.org>
2018-04-14 03:22:25 +0800

    Improve macOS compiler install script syntax

--- setup/macosx/compilers.sh
+++ setup/macosx/compilers.sh
@@ -6,13 +6,13 @@ fpc="fpc-3.0.4.intel-macosx"
 lazarus="lazarus-1.8.0-i686-macosx"
 
 if [ -n "${sourceforge_mirror-}" ]; then
-    mirror_string="&use_mirror=$sourceforge_mirror"
+    mirror_string="&use_mirror=${sourceforge_mirror}"
 fi
 
 if [ ! -x "$(command -v fpc 2>&1)" ]; then
-  wget "https://downloads.sourceforge.net/project/lazarus/Lazarus%20Mac%20OS%20X%20i386/Lazarus%201.8.0/$fpc.dmg?r=&ts=$(date +%s)${mirror_string-}" -O $fpc.dmg
-  hdiutil attach -quiet $fpc.dmg
-  pkgpath="$(hdiutil attach $fpc.dmg | grep Apple_HFS | awk '{ print $3 }')"
+  wget "https://downloads.sourceforge.net/project/lazarus/Lazarus%20Mac%20OS%20X%20i386/Lazarus%201.8.0/$fpc.dmg?r=&ts=$(date +%s)${mirror_string-}" -O "$fpc.dmg"
+  hdiutil attach -quiet "$fpc.dmg"
+  pkgpath="$(hdiutil attach "$fpc.dmg" | command awk "/Apple_HFS/ { print \$3 }")"
   sudo installer -pkg "$pkgpath/$fpc.pkg" -target /
   hdiutil unmount "$pkgpath"
   rm "$fpc.dmg"
@@ -21,7 +21,7 @@ fi
 if [ ! -x "$(command -v lazbuild 2>&1)" ]; then
   wget "https://downloads.sourceforge.net/project/lazarus/Lazarus%20Mac%20OS%20X%20i386/Lazarus%201.8.0/$lazarus.dmg?r=&ts=$(date +%s)${mirror_string-}" -O "$lazarus.dmg"
   hdiutil attach -quiet "$lazarus.dmg"
-  pkgpath="$(hdiutil attach "$lazarus.dmg" | grep Apple_HFS | awk '{ print $3 }')"
+  pkgpath="$(hdiutil attach "$lazarus.dmg" | command awk "/Apple_HFS/ { print \$3 }")"
   sudo installer -pkg "$pkgpath/lazarus.pkg" -target /
   hdiutil unmount "$pkgpath"
   rm "$lazarus.dmg"
