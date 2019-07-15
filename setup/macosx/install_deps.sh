20404de3ff0d969a8b4d9230cf89bea00884bcec
Peter Dave Hello <hsu@peterdavehello.org>
2019-07-15 21:00:00 +0800

    Separate lazarus version to var in setup/macosx/install_deps.sh

--- setup/macosx/install_deps.sh
+++ setup/macosx/install_deps.sh
@@ -3,6 +3,7 @@
 set -x
 set -e
 
+lazarus_ver="1.8.2"
 fpc="fpc-3.0.4.intel-macosx"
 lazarus="lazarus-1.8.2-i686-macosx"
 
@@ -11,7 +12,7 @@ if [ -n "${sourceforge_mirror-}" ]; then
 fi
 
 if [ ! -x "$(command -v fpc 2>&1)" ]; then
-  wget "https://downloads.sourceforge.net/project/lazarus/Lazarus%20Mac%20OS%20X%20i386/Lazarus%201.8.2/$fpc.dmg?r=&ts=$(date +%s)${mirror_string-}" -O "$fpc.dmg"
+  wget "https://downloads.sourceforge.net/project/lazarus/Lazarus%20Mac%20OS%20X%20i386/Lazarus%20${lazarus_ver}/$fpc.dmg?r=&ts=$(date +%s)${mirror_string-}" -O "$fpc.dmg"
   hdiutil attach -quiet "$fpc.dmg"
   pkgpath="$(hdiutil attach "$fpc.dmg" | command awk "/Apple_HFS/ { print \$3 }")"
   sudo installer -pkg "$pkgpath/$fpc.pkg" -target /
@@ -20,7 +21,7 @@ if [ ! -x "$(command -v fpc 2>&1)" ]; then
 fi
 
 if [ ! -x "$(command -v lazbuild 2>&1)" ]; then
-  wget "https://downloads.sourceforge.net/project/lazarus/Lazarus%20Mac%20OS%20X%20i386/Lazarus%201.8.2/$lazarus.dmg?r=&ts=$(date +%s)${mirror_string-}" -O "$lazarus.dmg"
+  wget "https://downloads.sourceforge.net/project/lazarus/Lazarus%20Mac%20OS%20X%20i386/Lazarus%20${lazarus_ver}/$lazarus.dmg?r=&ts=$(date +%s)${mirror_string-}" -O "$lazarus.dmg"
   hdiutil attach -quiet "$lazarus.dmg"
   pkgpath="$(hdiutil attach "$lazarus.dmg" | command awk "/Apple_HFS/ { print \$3 }")"
   sudo installer -pkg "$pkgpath/lazarus.pkg" -target /
