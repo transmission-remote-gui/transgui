5a996a6a8d4e323844db43d8c283d0e398d0eb06
Peter Dave Hello <hsu@peterdavehello.org>
2019-07-20 21:56:52 +0800

    Refactor Windows build script to catch errors

--- setup/win/make_setup.bat
+++ setup/win/make_setup.bat
@@ -17,7 +17,9 @@ if "%2" NEQ "" (
 set path=%LAZARUS_DIR%;%LAZARUS_DIR%\fpc\3.0.4\bin\i386-win32;%path%
 
 lazbuild -B ../../transgui.lpi
+if errorlevel 1 goto err
 make -C ../.. clean
+if errorlevel 1 goto err
 make -C ../.. all
 if errorlevel 1 goto err
 
