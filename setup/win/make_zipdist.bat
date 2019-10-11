a04256daf3a40a0ecbd3f5ea89a0042d135ec162
Peter Dave Hello <hsu@peterdavehello.org>
2019-10-11 19:26:27 +0800

    Add Windows script to build zip tarball release

---
+++ setup/win/make_zipdist.bat
@@ -0,0 +1,29 @@
+@echo off
+
+echo "Usage: %~nx0 <lazarus_dir>"
+
+if "%1" NEQ "" (
+    set "LAZARUS_DIR=%1"
+) else (
+    set "LAZARUS_DIR=C:\lazarus"
+)
+
+set path=%LAZARUS_DIR%;%LAZARUS_DIR%\fpc\3.0.4\bin\i386-win32;%path%
+
+lazbuild -B ../../transgui.lpi
+if errorlevel 1 goto err
+make -C ../.. clean
+if errorlevel 1 goto err
+make -C ../.. all
+if errorlevel 1 goto err
+upx --best ../../transgui.exe
+if errorlevel 1 goto err
+make -C ../.. zipdist
+if errorlevel 1 goto err
+
+pause
+exit /b 0
+
+:err
+pause
+exit /b 1
